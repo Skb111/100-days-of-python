e9ef46087434ce8e5a633472c96f383687b87e44
skb <webemail655@gmail.com>
2023-03-06 22:56:15 +0100

    day2

--- day1.py
+++ day1.py
@@ -1,3 +1,8 @@
 print("Day 1 - python print function")
 print("The function declared like this:")
 print("print('what to print')")
+
+print("Hello " + input("What is your name?"))
+namelen = len(input("Intercontinental"))
+print(namelen)
+print(len(input("Intercontinental")))
---
+++ day2.py
@@ -0,0 +1,5 @@
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill $"))
+perc = int(input("What percentage would you like to give? 10, 12, 15?"))
+people = int(input("How many people to split?"))
+output = print("Each person should pay: $")
